f1e24b0259f95b9ed92f89263fa31c989569ad24
Saransh <saranshmain17@gmail.com>
2023-10-13 17:23:32 +0530

    recylcer centre layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
 
+    //lottie files
+    implementation("com.airbnb.android:lottie:3.4.0")
 }
