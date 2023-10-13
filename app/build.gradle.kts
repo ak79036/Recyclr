b9580b4d6c0b8e01c59002e7129bda5b1ba1c86e
Aritra <aritrabiswas9788@gmail.com>
2023-10-13 18:39:28 +0530

    MapBox SDK put

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,19 +44,22 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
     // navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
 
     //lottie files
-    implementation("com.airbnb.android:lottie:3.4.0")
+    implementation("com.airbnb.android:lottie:6.1.0")
+    //mapBox
+    implementation ("com.mapbox.maps:android:10.16.0")
+    implementation ("com.mapbox.navigation:android:2.16.0")
+    implementation ("com.mapbox.search:mapbox-search-android:1.0.0-rc.6")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,22 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven{setUrl("https://jitpack.io")}
+        maven {
+            setUrl("https://api.mapbox.com/downloads/v2/releases/maven")
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            credentials {
+                // Do not change the username below.
+                // This should always be `mapbox` (not your username).
+                username = "mapbox"
+                // Use the secret token you stored in gradle.properties as the password
+                password = "sk.eyJ1IjoiYXJpdHJhOTc4OCIsImEiOiJjbG16MmRyM2YwNHFpMm9tdmVhczB3N3lxIn0.M6EB7L_JPbxuM9T_i0ExFA"
+            }
+        }
     }
+
 }
 
 rootProject.name = "WasteMangement"
